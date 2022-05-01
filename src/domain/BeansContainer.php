fe2911950b449981446998c75a0657f45083a86d
Francisco Giraldo <fcog@iMac-de-Francisco.local>
2022-05-01 11:52:02 +0200

    Removed unneeded comments

--- src/domain/BeansContainer.php
+++ src/domain/BeansContainer.php
@@ -15,13 +15,7 @@ class BeansContainer implements BeansContainerInterface
     }
 
     /**
-     * Adds beans to the container
-     *
-     * @param int $numSpoons number of spoons of beans
-     *
-     * @return void
      * @throws ContainerFullException
-     *
      */
     public function addBeans(int $numSpoons): void
     {
@@ -32,13 +26,6 @@ class BeansContainer implements BeansContainerInterface
         $this->currentNumSpoons += $numSpoons;
     }
 
-    /**
-     * Use $numSpoons from the container
-     *
-     * @param int $numSpoons number of spoons of beans
-     *
-     * @return int number of bean spoons used
-     */
     public function useBeans(int $numSpoons): int
     {
         $this->currentNumSpoons -= $numSpoons;
@@ -46,11 +33,6 @@ class BeansContainer implements BeansContainerInterface
         return $numSpoons;
     }
 
-    /**
-     * Returns the number of spoons of beans left in the container
-     *
-     * @return int
-     */
     public function getBeans(): int
     {
         return $this->currentNumSpoons;
